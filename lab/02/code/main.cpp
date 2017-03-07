3864145f980ac1a73b1e3af0dac6490792b92d06
Maciej Faron <supremacey@icloud.com>
2017-03-07 17:35:07 +0100

    [Lab02] Task 3 and 4 done.

--- lab/02/code/main.cpp
+++ lab/02/code/main.cpp
@@ -2,8 +2,28 @@
 #include "rational.h"
 #include "matrix.h"
 
+void task04() {
+   rational r1{1,2};
+   rational r2{1,3};
+   rational r3{-2,7};
+   rational r4{2,8};
+
+   rational r5{-1,3};
+   rational r6{2,7};
+   rational r7{2,5};
+   rational r8{-1,7};
+
+   matrix m1 {{r1,r2},{r3,r4}};
+   matrix m2 {{r5,r6},{r7,r8}};
+
+   std::cout << m1 * m2 << "\n";
+   std::cout << m1.inverse() << std::endl;
+   std::cout << m1 * m1.inverse() << std::endl;
+}
+
 int main( int argc, char* argv [ ] )
 {
+#if 0
    rational r1( 2, 6 );
    rational r2( 4, 3 ); 
    rational r3( 5,6 );
@@ -21,6 +41,7 @@ int main( int argc, char* argv [ ] )
 
    std::cout << m1. adjugate( ) << "\n";
    std::cout << m1. inverse( ) * m1 << "\n";
-
+#endif
+	task04();
 }
 
--- lab/02/code/rational.cpp
+++ lab/02/code/rational.cpp
@@ -1,28 +1,106 @@
-
+#include <cmath>
+#include <stdexcept>
 #include "rational.h"
 
+#define POSITIVE 0
+#define NEGATIVE 1
 // Complete these methods:
 
-#if 0 
+#if 1 
 
-int rational::gcd( int n1, int n2 );
+int rational::gcd( int n1, int n2 ) {
+    int a, b, r;
 
-void rational::normalize( );
+    if (n1 > n2 ) { a = n1; b = n2; }
+    else {a = n2; b = n1;}
 
-rational operator - ( rational r );
+    while ((r = a%b) != 0) {
+       a = b;
+       b = r;
+    }
 
-rational operator + ( const rational& r1, const rational& r2 );
+    return b;
+}
 
-rational operator - ( const rational& r1, const rational& r2 );
+void rational::normalize( ) {
+	// now this is infinity or undefined
+	if (denum == 0 )
+		throw std::domain_error("Division by 0 (denum is 0).");
 
-rational operator * ( const rational& r1, const rational& r2 );
+	// if num is 0 there is nothing we have to do	
+	if (num == 0)
+		return;
 
-rational operator / ( const rational& r1, const rational& r2 );
+	// how sign is preserved
+	auto _sign = POSITIVE;
+	if ((num * denum) > 0) 
+		_sign = POSITIVE;
+	else 
+		_sign = NEGATIVE;
 
-bool operator == ( const rational& r1, const rational& r2 );
-bool operator != ( const rational& r1, const rational& r2 );
+	num = std::abs(num);
+	denum = std::abs(denum);
 
-std::ostream& operator << ( std::ostream& stream, const rational& r ) ;
+	auto _divisor = rational::gcd(num, denum);
+	num /= _divisor;
+	denum /= _divisor;
+
+	// restore of sign
+	if (_sign == NEGATIVE)
+		num *= -1;
+}
+
+rational operator - ( rational r ) {
+	auto _sign = ((r.num * r.denum) < 0) ? NEGATIVE : POSITIVE;
+	r.num = std::abs(r.num);
+	r.denum = std::abs(r.denum);
+
+	if ( _sign==POSITIVE )
+		r.num *= -1;
+
+	return r;
+}
+
+rational operator + ( const rational& r1, const rational& r2 ) {
+	rational _result = { r1.num * r2.denum + r2.num * r1.denum , r1.denum * r2.denum};
+	_result.normalize();
+	return _result;
+}
+
+rational operator - ( const rational& r1, const rational& r2 ) {
+	rational _result = { r1.num * r2.denum - r2.num * r1.denum , r1.denum * r2.denum};
+	_result.normalize();
+	return _result;
+}
+
+rational operator * ( const rational& r1, const rational& r2 ) {
+	return rational{r1.num * r2.num, r1.denum * r2.denum};
+}
+
+rational operator / ( const rational& r1, const rational& r2 ) {
+	return rational{r1.num * r2.denum, r1.denum * r2.num};
+}
+
+bool operator == ( const rational& r1, const rational& r2 ) {
+	auto _r1 = r1;
+	auto _r2 = r2;
+	_r1.normalize();
+	_r2.normalize();
+	
+	if (r1.denum == r2.denum) return r1.num == r2.num;
+	if (r1.num == r2.num) return r1.denum == r2.denum;
+
+	return false;
+}
+
+bool operator != ( const rational& r1, const rational& r2 ) {
+	return !(r1 == r2);
+}
+
+std::ostream& operator << ( std::ostream& stream, const rational& r ) {
+	stream << r.num << "/" << r.denum;
+	return stream;
+}
 
 #endif
 
